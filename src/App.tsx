69fadc95f5750e48d1509ef4e7f696340d5aa188
Jonnes Nascimento <jonnesfn@ipt.br>
2025-10-23 23:14:27 -0300

    Input text and movie list implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Banner from './components/Banner'
 import Header from './components/Header'
+import MovieSection from './components/MovieSection'
 
 function App() {
   return (
     <>
       <Header />
       <Banner src='./banner.png' alt='Banner' />
+      <MovieSection />
     </>
   )
 }
